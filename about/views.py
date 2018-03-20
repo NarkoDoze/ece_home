67cb76f6eab0473a5bad66f8aacd3b05ae54ab16
LLJE <rlaxortn13@naver.com>
2018-03-20 20:10:11 +0900

    modify "about" page

--- about/views.py
+++ about/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request, 'about.html')
+    params = {'title': 'About AROUND'}
+    return render(request, 'about.html', params)
