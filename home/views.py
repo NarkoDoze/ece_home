d33b850aa45a156cdf1d400fab47335ccba51124
KimYeonJun <yeonjunkim@unist.ac.kr>
2018-03-26 19:05:26 +0900

    index page connected with notice

--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from event.models import Event
+from notice.models import Post
 
 # Create your views here.
 def index(request):
     params = {'title': 'ECE Student Council AROUND'}
+
+    events = Event.objects.all().order_by('-id')[:3]
+    params['firstEvent'] = events[0]
+    params['events'] = events[1:]
+
+    posts = Post.objects.all().order_by('-id')[:5]
+    for post in posts:
+        post.date = str(post.created_at.month) + "-" + str(post.created_at.day)
+    params['posts'] = posts
+    params['announcelink'] = '/announce'
+
     return render(request, 'index.html', params)
